2733408a662b36370999915983ff54ed432fa2ed
ildickRR <mexmat307@mail.ru>
2026-03-11 10:32:45 +0300

    salf

--- ex01/benchmark.py
+++ ex01/benchmark.py
@@ -7,16 +7,20 @@ def main():
 
     time1 = timeit.timeit(lambda: find_app(email_list), number=9000000)
     time2 = timeit.timeit(lambda: find_comp(email_list), number=9000000)  
+    time3 = timeit.timeit(lambda: find_map(email_list), number=9000000)  
 
-    if(time1 <= time2):
+
+    if(time1 <= time2 and time1 <= time3):
         print("it is better to use a loop")
-    else:
+    elif time2 < time1 and time2 < time3:
         print("it is better to use a list comprehension")
+    else:
+        print("it is better to use a map")
 
-    times = [time1, time2]
+    times = [time1, time2, time3]
     times.sort()
 
-    print(f"{times[0]} vs {times[1]}")
+    print(f"{times[0]} vs {times[1]} vs {times[2]}")
 
 
 def find_app(email_list):
@@ -31,6 +35,9 @@ def find_app(email_list):
 def find_comp(email_list):
     return [email for email in email_list if email.endswith('@gmail.com')]
 
+
+def find_map(email_list):
+    return list(map(lambda email: email if email.endswith('@gmail.com') else None, email_list))
 
 if __name__ == "__main__":
     main()
